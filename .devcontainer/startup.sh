7586fd9d202d0f60dc756d629c524e7a5d034057
Patrick Coxall <mr.coxall@mths.ca>
2025-04-25 19:01:26 +0000

    update

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -4,6 +4,3 @@
 cp ./.devcontainer/.bashrc ~/.bashrc
 # shellcheck disable=SC1090
 source ~/.bashrc
-
-# change welcome message
-sudo cp ./.devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
